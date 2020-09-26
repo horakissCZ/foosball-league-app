1dd0aa51b74a4ca35ec4d81099bc0b042def781c
Jakub Horák <jakub.horak@closeit.co>
2020-09-26 21:34:29 +0200

    Init spring boot project

---
+++ src/main/java/com/horak/foosballleague/FoosballLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.horak.foosballleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoosballLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoosballLeagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/horak/foosballleague/FoosballLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horak.foosballleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoosballLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
